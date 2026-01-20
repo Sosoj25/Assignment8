16777363c79f23cd03c54b1c9f5ad13897cabe41
Sosoj25 <karw5799@gmail.com>
2026-01-20 18:15:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./Login";
+import Register from "./Register";
+
+function App() {
+  return (
+    <div>
+      <h1>App Loaded</h1>
+      <Login />
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
